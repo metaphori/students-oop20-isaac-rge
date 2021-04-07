ec5058cb4e30409070c3daa9a7bbda1b5c0841ba
CavDev26 <lorenzo.cavallucci4@studio.unibo.it>
2021-04-07 20:00:36 +0200

    Added and implemented BulletGraphicComponent.

---
+++ src/main/java/ryleh/view/other/BulletGraphicComponent.java
@@ -0,0 +1,31 @@
+package ryleh.view.other;
+
+import javafx.geometry.Point2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import ryleh.view.GraphicComponent;
+import ryleh.view.Textures;
+
+public class BulletGraphicComponent implements GraphicComponent{
+
+	private Rectangle rectangle;
+	
+	public BulletGraphicComponent() {
+		rectangle = new Rectangle(20, 20);
+		rectangle.setFill(Color.BLACK);
+	}
+	@Override
+	public void render(Point2D position, int deltaTime) {
+		rectangle.setX(position.getX());
+		rectangle.setY(position.getY());
+	}
+
+	@Override
+	public void onAdded(Scene scene) {
+		Parent root = scene.getRoot();
+        ((AnchorPane) root).getChildren().add(rectangle);
+	}
+}

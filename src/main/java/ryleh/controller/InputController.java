fd1f271812104e95afbc75b2e1c92068393d534a
Francesco <francesco.magnani14@studio.unibo.it>
2021-04-14 20:40:04 +0200

    InputController second hotfix

--- src/main/java/ryleh/controller/InputController.java
+++ src/main/java/ryleh/controller/InputController.java
@@ -69,10 +69,10 @@ public class InputController {
 				this.currentDir = isMoving() ? this.currentDir : Direction.IDLE;
 			        break;
 			case W: isMoveUp = false;
-					this.currentDir = isMoving() ? this.currentDir : Direction.IDLE;
+				this.currentDir = isMoving() ? this.currentDir : Direction.IDLE;
 				break;
 			case S: isMoveDown = false;
-					this.currentDir = isMoving() ? this.currentDir : Direction.IDLE;
+				this.currentDir = isMoving() ? this.currentDir : Direction.IDLE;
 				break;
 			case L: this.newLevel = false;
 				break;
@@ -94,33 +94,18 @@ public class InputController {
 				this.shoot(this.physics.getDirection());
 			}
 		}
+		if (this.currentDir.equals(Direction.UP) && !isMoveUp) {
+		    this.currentDir = Direction.IDLE;
+		} else if (this.currentDir.equals(Direction.DOWN) && !isMoveDown) {
+                    this.currentDir = Direction.IDLE;
+                } if (this.currentDir.equals(Direction.LEFT) && !isMoveLeft) {
+                    this.currentDir = Direction.IDLE;
+                } else if (this.currentDir.equals(Direction.RIGHT) && !isMoveRight) {
+                    this.currentDir = Direction.IDLE;
+                }
 		this.move(this.currentDir);
-//		if (this.notMoving()) {
-//			this.physics.setVelocity(Direction.IDLE.getPoint());
-//			this.physics.setDirection(Direction.IDLE);
-//			this.graphic.setDirection(Direction.IDLE);
-//			this.physics.resetBlocked();
-//		}
 	}
 
-	private void setMoving(final KeyCode key) {
-//		switch (key) {
-//		case A: this.currentDir = Direction.LEFT;
-//			break;
-//		case D: this.currentDir = Direction.RIGHT;
-//			break;
-//		case W: this.currentDir = Direction.UP;
-//			break;
-//		case S: this.currentDir = Direction.DOWN;
-//			break;
-//		case L: this.newLevel = isMoving;
-//			break;
-//		case SPACE: this.isShooting = isMoving;
-//		break;
-//		default:
-//			break;
-//		}
-	}
 	private void shoot(final Direction direction) {
 		((ShootingComponent) player.getGameObject().getComponent(ShootingComponent.class).get())
 		    .shoot(direction.getPoint());
@@ -134,10 +119,6 @@ public class InputController {
                             this.physics.resetBlocked();
                         }
 			this.graphic.setDirection(direction);
-	}
-	
-	private boolean notMoving() {
-		return !this.isMoveDown && !this.isMoveUp && !this.isMoveLeft && !this.isMoveRight;
 	}
 	private boolean isMoving() {
 	    return this.isMoveDown || this.isMoveLeft || this.isMoveRight || this.isMoveUp;

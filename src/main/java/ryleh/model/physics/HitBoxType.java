9ce462aa18bccc7c907472a2adf05675e2de185b
CipStr <razvancipr.stricescu@studio.unibo.it>
2021-04-18 18:04:49 +0200

    Changed value for item hitbox radius

--- src/main/java/ryleh/model/physics/HitBoxType.java
+++ src/main/java/ryleh/model/physics/HitBoxType.java
@@ -3,13 +3,13 @@ package ryleh.model.physics;
  * Enum used to determine hitbox radius of different entities.
  */
 public enum HitBoxType {
-    PLAYER (70),
-    BULLET (5),
-    ROCK (75),
-    ITEM (30),
-    FIRE (45),
-    DOOR (75),
-    ENEMY (55);
+    PLAYE(70),
+    BULLET(5),
+    ROCK(75),
+    ITEM(30),
+    FIRE(40),
+    DOOR(75),
+    ENEMY(55);
     private  int boxRadius;
     HitBoxType(int radius) {
         boxRadius = radius;

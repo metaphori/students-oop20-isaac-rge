9384dd7a2e6404bea3c20f0b5f11b45272c59789
ThomasCapelli <thomas.capelli@studo.unibo.it>
2021-04-18 11:05:49 +0200

    resolved checkstyle errors

--- src/main/java/ryleh/controller/events/ItemPickUpEvent.java
+++ src/main/java/ryleh/controller/events/ItemPickUpEvent.java
@@ -3,10 +3,7 @@ package ryleh.controller.events;
 import java.util.Random;
 
 import ryleh.controller.core.GameState;
-import ryleh.model.GameObject;
 import ryleh.model.Type;
-import ryleh.model.components.HealthIntComponent;
-import ryleh.model.components.ShootingComponent;
 import ryleh.model.items.FireSpeedItem;
 import ryleh.model.items.HealItem;
 import ryleh.model.items.Item;
@@ -14,61 +11,39 @@ import ryleh.model.items.MaxHealthItem;
 import ryleh.view.other.ItemGraphicComponent;
 
 public class ItemPickUpEvent implements Event {
-	private final GameObject target;
-	private HealthIntComponent health;
-	private Item item;
-	/**
-	 * Constructor for the collision with an Item.
-	 * @param target Player, only this game object can collide with items
-	 */
-	public ItemPickUpEvent(final GameObject target) {
-		this.target = target;
-	}
+    private Item item;
 
-	/**
-	 * {@inheritDoc}
-	 * Starts the opening animation of the item
-	 */
-	@Override
-	public void handle(final GameState state) {
-		randomItem(state);
-		((ItemGraphicComponent) state.getEntityByType(Type.ITEM).get().getView()).setAnimPlayed();
-	}
-	/**
-	 * Method to generate a random buff with pseudo probability.
-	 */
-	private void randomItem(final GameState state) {
-		Random random = new Random();
-		switch (random.nextInt(3)) {
-		case 0: item = new HealItem();
-		    state.getView().getItemPickUp().setText("Item effect: health up!");
-		    state.getView().playFt();
-			break;
-		case 1: item = new MaxHealthItem();
-		    state.getView().getItemPickUp().setText("Item effect: max health up!");
-		    state.getView().playFt();
-			break;
-		case 2: item = new FireSpeedItem();
-			break;
-		default:
-			break;
-		}
-		item.apply(state);
-	}
-	/**
-	 * Heal the target and updates game UI with item effect.
-	 */
-//	private void healthUp() {
-//		gs.getView().getItemPickUp().setText("Item effect: health up!");
-//		gs.getView().playFt();
-//		health.heal(1);
-//	}
-//	/**
-//	 * Increase max health of the target and updates game UI with item effect.
-//	 */
-//	private void maxHealthUp() {
-//		gs.getView().getItemPickUp().setText("Item effect: max health up!");
-//		gs.getView().playFt();
-//		health.setMaxHp(health.getMaxHp() + 1);
-//	}
+    /**
+     * {@inheritDoc} Starts the opening animation of the item
+     */
+    @Override
+    public void handle(final GameState state) {
+        randomItem(state);
+        ((ItemGraphicComponent) state.getEntityByType(Type.ITEM).get().getView()).setAnimPlayed();
+    }
+
+    /**
+     * Method to generate a random buff with pseudo probability.
+     */
+    private void randomItem(final GameState state) {
+        Random random = new Random();
+        switch (random.nextInt(3)) {
+        case 0:
+            item = new HealItem();
+            state.getView().getItemPickUp().setText("Item effect: health up!");
+            state.getView().playFt();
+            break;
+        case 1:
+            item = new MaxHealthItem();
+            state.getView().getItemPickUp().setText("Item effect: max health up!");
+            state.getView().playFt();
+            break;
+        case 2:
+            item = new FireSpeedItem();
+            break;
+        default:
+            break;
+        }
+        item.apply(state);
+    }
 }
--- src/main/java/ryleh/model/components/CollisionComponent.java
+++ src/main/java/ryleh/model/components/CollisionComponent.java
@@ -8,47 +8,47 @@ import ryleh.model.Type;
 import ryleh.model.World;
 
 public class CollisionComponent extends Component {
-	
-	private final Type type;
-	private boolean hasAlreadyColided = false;
-	//private boolean doorCollidable;
-	
-	/**
-	 * Add a component to check the collision between enemies and player.
-	 * @param world
-	 * @param type The type of the concerned GameObject 
-	 */
-	public CollisionComponent(final World world, final Type type) {
-		super(world);
-		this.type = type;
-		this.hasAlreadyColided = false;
-		//this.doorCollidable = false;
-	}
-	
-	public void setDoorCollidable() {
-		//this.doorCollidable = true;
-	}
-	/**
-	 * A method which checks for collisions with other GameObjects and notifies world instance in case of event.
-	 */
-	public void onUpdate(final double deltaTime) {
-		super.onUpdate(deltaTime);
-		if (!this.hasAlreadyColided) {
-			Optional<GameObject> colliding = world.getGameObjects().stream()
-					.filter(obj -> obj.getType().equals(Type.PLAYER))
-					.filter(obj -> obj.getHitBox().isCollidingWith(object.getHitBox()))
-					.findFirst();
-			if (colliding.isPresent()) {
-				if (type.equals(Type.ITEM)) {
-					world.notifyWorldEvent(new ItemPickUpEvent(colliding.get()));
-					this.hasAlreadyColided = true;
-				/*} else if (type.equals(Type.DOOR)) {
-					world.notifyWorldEvent(new NewLevelEvent());
-				*/} else {
-					world.notifyWorldEvent(new EnemyCollisionEvent(colliding.get()));
-				}
-		    }
-		}
-	}
+
+    private final Type type;
+    private boolean hasAlreadyColided = false;
+    // private boolean doorCollidable;
+
+    /**
+     * Add a component to check the collision between enemies and player.
+     * 
+     * @param world
+     * @param type  The type of the concerned GameObject
+     */
+    public CollisionComponent(final World world, final Type type) {
+        super(world);
+        this.type = type;
+        this.hasAlreadyColided = false;
+        // this.doorCollidable = false;
+    }
+
+    public void setDoorCollidable() {
+        // this.doorCollidable = true;
+    }
+
+    /**
+     * A method which checks for collisions with other GameObjects and notifies
+     * world instance in case of event.
+     */
+    public void onUpdate(final double deltaTime) {
+        super.onUpdate(deltaTime);
+        if (!this.hasAlreadyColided) {
+            Optional<GameObject> colliding = world.getGameObjects().stream()
+                    .filter(obj -> obj.getType().equals(Type.PLAYER))
+                    .filter(obj -> obj.getHitBox().isCollidingWith(object.getHitBox())).findFirst();
+            if (colliding.isPresent()) {
+                if (type.equals(Type.ITEM)) {
+                    world.notifyWorldEvent(new ItemPickUpEvent());
+                    this.hasAlreadyColided = true;
+                } else {
+                    world.notifyWorldEvent(new EnemyCollisionEvent(colliding.get()));
+                }
+            }
+        }
+    }
 
 }

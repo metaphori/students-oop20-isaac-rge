cacdf4683664dd02049c533176274be43c6ae057
Francesco <francesco.magnani14@studio.unibo.it>
2021-04-23 18:40:18 +0200

    Setting up Gradle tests with junit

--- src/test/java/test/java/WorldBoundsTest.java
+++ src/test/java/test/java/WorldBoundsTest.java
@@ -1,7 +1,8 @@
-package test;
+package test.java;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
